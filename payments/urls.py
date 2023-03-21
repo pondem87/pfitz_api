50d9b1a39a40f3e92a3f9373baebd2b9bb2a29f3
tendai pfidze <pfidzetendai@gmail.com>
2023-03-21 10:47:07 +0200

    initial commit v2

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('status/', views.UpdatePaymentAPIView.as_view()),
+    path('status/', views.UpdatePaymentAPIView.as_view()),
     path('', views.PaymentListAPIView.as_view()),
     path('pay/', views.InitiatePaymentAPIView.as_view()),
     path('<str:uuid>/', views.GetPaymentAPIView.as_view()),
