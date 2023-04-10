d37bcaf371d3fc1ef22f62cbc4680eac924d3ef7
tendai pfidze <pfidzetendai@gmail.com>
2023-04-10 19:24:06 +0200

    fixed daphne issue

--- pfitz_api/asgi.py
+++ pfitz_api/asgi.py
@@ -9,16 +9,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pfitz_api.settings')
+
 from django.core.asgi import get_asgi_application
+asgi_app = get_asgi_application()
 from .ws_auth_token import CustomAuthMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from zimgpt.routing import urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pfitz_api.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": asgi_app,
     "websocket": AllowedHostsOriginValidator(
         CustomAuthMiddleware(
             URLRouter(urlpatterns)
