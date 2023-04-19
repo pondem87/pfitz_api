d96ad9d5fad087ce6abd70d87b1568ab733bebdb
tendai pfidze <pfidzetendai@gmail.com>
2023-04-19 05:04:18 +0200

    removed CSRF token junk conflicting with deployment

--- pfitz_api/settings.py
+++ pfitz_api/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     # cors middleware goes above commonmiddleware
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
