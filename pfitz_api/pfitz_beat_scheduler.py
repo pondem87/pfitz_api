4ce7be99ee7e7edd7f527f1cdc258de641b0c035
tendai pfidze <pfidzetendai@gmail.com>
2024-03-03 09:21:16 +0200

    updated pfitz_beat_scheduler

--- pfitz_api/pfitz_beat_scheduler.py
+++ pfitz_api/pfitz_beat_scheduler.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 
 class PfitzRabbitMQLockedScheduler(DatabaseScheduler):
 
+    ## this option is set when running multiple servers in an autoscaling group
     multi_instance = config("CELERY_BEAT_MULTI_INSTANCE", cast=bool, default=False)
 
     def initLockConnection(self):
@@ -17,6 +18,7 @@ class PfitzRabbitMQLockedScheduler(DatabaseScheduler):
             logger.debug("Connecting to rabbitMQ")
             # SSL Context for TLS configuration of Amazon MQ for RabbitMQ
 
+            # set up a connection to rabbitmq using pika
             if config("PIKA_SCHEME") == "SSL":
                 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
                 ssl_context.set_ciphers('ECDHE+AESGCM:!ECDSA')
@@ -38,14 +40,21 @@ class PfitzRabbitMQLockedScheduler(DatabaseScheduler):
 
     def initLockQueue(self):
         try:
+            # set up an exclusive queue in radditmq
             logger.debug("Declaring exclusive lock")
+
+            # check if we have an open channel
             if not hasattr(self, 'lock_channel'):
                 logger.debug("Preparing channel")
                 self.lock_channel = self.lock_connection.channel()
             elif self.lock_channel.is_closed:
                 logger.debug("Reopening closed channel")
+                # open a channel if we do not have one
                 self.lock_channel = self.lock_connection.channel()
+
+            # if channel established then declarethe exclusive queue
             self.lock_channel.queue_declare(queue=self.lock_queue_name, exclusive=True)
+
             return True
         except Exception as error:
             logger.debug('Failed to declare exclusive queue: %s', str(error))
@@ -53,19 +62,18 @@ class PfitzRabbitMQLockedScheduler(DatabaseScheduler):
         
     def doesLockQueueExist(self):
         try:
-            # Declare the queue
+            # declare the queue, this will fail if another instance already has the queue
             logger.debug("Check if lock is exists")
             self.lock_channel.queue_declare(queue=self.lock_queue_name, passive=True)
             return True
         except Exception as error:
-            # If the broker closes the channel, the queue doesn't exist
+            # if the broker closes the channel, the queue doesn't exist
             logger.debug('Lock queue doesnt exist: %s', str(error))
-            
             return False
             
     def hasLock(self):
         try:
-            # Declare the queue
+            # Declare the queue, this will fail if another instance already has the queue
             logger.debug("Check if lock is owned by this instance")
             self.lock_channel.queue_declare(queue=self.lock_queue_name, passive=True)
             return True
@@ -79,16 +87,20 @@ class PfitzRabbitMQLockedScheduler(DatabaseScheduler):
     def tick(self, *args, **kwargs):
 
         if PfitzRabbitMQLockedScheduler.multi_instance:
+            # when we are running a multi server deployment
 
             if not hasattr(self, 'lock_tick_counter'):
+                # these attributes allow us to increase interval between ticks and preserve compute resources
                 self.lock_tick_counter = 0
                 self.lock_check_tick_interval = config("PFITZ_SCHEDULER_LOCK_CHECK_INTERVAL", cast=int, default=5)
                 self.can_tick = False
             elif self.lock_tick_counter < self.lock_check_tick_interval:
                 self.lock_tick_counter += 1
             else:
+                # when counter overflows then its time to tick.
                 self.lock_tick_counter = 0
 
+                # check if channel and connection exist and create them if not exists
                 if not hasattr(self, 'lock_connection'):
                     self.lock_queue_name = 'pfitz-celerybeat-lock'
                     if self.initLockConnection():
@@ -97,6 +109,7 @@ class PfitzRabbitMQLockedScheduler(DatabaseScheduler):
                 else:
                     self.initLockQueue()
 
+                # check if this instance has the lock
                 if self.hasLock():
                     logger.debug("Lock obtained, can now tick")
                     self.can_tick = True
@@ -104,15 +117,17 @@ class PfitzRabbitMQLockedScheduler(DatabaseScheduler):
                     logger.debug("Lock unavailable, stop ticking")
                     self.can_tick = False
 
-
+            # run tick if lock established
             if self.can_tick:
                 logger.debug("MULTI SCHEDULER TICKING")
                 super().tick(*args, **kwargs)
         
         else:
+            # in single server instance deployment just go ahead and tick
             logger.debug("SINGLE SCHEDULER TICKING")
             super().tick(*args, **kwargs)
     
+    # clean up the mess
     def __del__(self):
         try:
             logger.debug("Closing a connection")
